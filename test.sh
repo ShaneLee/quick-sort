4d0676663cb49569f7b8b8201930ed6c4d8c9f8d
Shane Lee <me@shanel.ee>
2020-06-16 12:10:33 +0100

    Add test function

--- test.sh
+++ test.sh
@@ -4,11 +4,13 @@ expected="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
 
 python_result=$(python3 python/quick-sort.py ${test_arr[@]} 2>&1)
 
-echo "${python_result}"
-echo "${expected}"
+test_sort() {
+  result=$(eval "$1 ${test_arr[@]} 2>&1")
+  if [ "$result" == "$expected" ]; then
+    echo "Test passed"
+  else
+    echo "Test failed, expected $expected got: $result"
+  fi
+}
 
-if [ "$python_result" == "$expected" ]; then
-  echo "Test passed"
-else
-  echo "Test failed"
-fi
+test_sort "python3 python/quick-sort.py"
