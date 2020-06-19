2e76753c7f62b7410dce77d70f0f852f7dd3fe4b
Shane Lee <me@shanel.ee>
2020-06-19 19:22:32 +0100

    Remove unneeded line from test.

--- test.sh
+++ test.sh
@@ -3,8 +3,6 @@ test_arr="1,5,4,3,7,6,2,9,8,10"
 expected="[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]"
 expected2="1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
 
-python_result=$(python3 python/quick-sort.py ${test_arr[@]} 2>&1)
-
 test_sort() {
   result=$(eval "$1 ${test_arr[@]} 2>&1")
   if [ "$result" == "$expected" ]  || [  "$result" == "$expected2" ]; then
